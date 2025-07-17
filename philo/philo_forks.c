ef4af4e0e729a29cba04d64f928c47a883ae0d92
DKOK01 <aymansadik2005@gmail.com>
2025-07-17 13:31:29 +0100

    refactor: consolidate fork order logic into a single function for clarity

--- philo/philo_forks.c
+++ philo/philo_forks.c
@@ -6,7 +6,7 @@
 /*   By: aysadeq <aysadeq@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 17:00:46 by aysadeq           #+#    #+#             */
-/*   Updated: 2025/07/15 20:01:32 by aysadeq          ###   ########.fr       */
+/*   Updated: 2025/07/17 12:49:21 by aysadeq          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,7 @@ static void	handle_single_philo(t_philo *philo)
 	pthread_mutex_unlock(&philo->data->forks[0]);
 }
 
-static void	get_fork_order_odd(t_philo *philo, int *first, int *second)
-{
-	if (philo->id == philo->data->num_philos)
-	{
-		*first = philo->right_fork;
-		*second = philo->left_fork;
-	}
-	else
-	{
-		*first = philo->left_fork;
-		*second = philo->right_fork;
-	}
-}
-
-static void	get_fork_order_even(t_philo *philo, int *first, int *second)
+static void	get_fork_order(t_philo *philo, int *first, int *second)
 {
 	if (philo->left_fork < philo->right_fork)
 	{
@@ -58,10 +44,7 @@ int	take_forks(t_philo *philo)
 		handle_single_philo(philo);
 		return (0);
 	}
-	if (philo->data->num_philos % 2 == 1)
-		get_fork_order_odd(philo, &first_fork, &second_fork);
-	else
-		get_fork_order_even(philo, &first_fork, &second_fork);
+	get_fork_order(philo, &first_fork, &second_fork);
 	pthread_mutex_lock(&philo->data->forks[first_fork]);
 	print_status(philo, FORK);
 	if (is_simulation_over(philo->data))
